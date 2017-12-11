509f4ae6d741460d2cf76ef64293b5d446e38549
Benny van Reeven <bvanreeven@gmail.com>
2017-12-11 20:35:36 +0100

    Use new parsimmon typings

--- src/day9/parser-parsimmon.ts
+++ src/day9/parser-parsimmon.ts
@@ -2,14 +2,14 @@ import { IGroup, IGarbage, Item } from "./helper";
 
 import * as Parsimmon from "parsimmon";
 
-const language = Parsimmon.createLanguage/*<{
+const language = Parsimmon.createLanguage<{
   Item: Item;
   Group: IGroup;
   Garbage: IGarbage;
-}>*/({
-  Item: r => Parsimmon.alt(r!.Group, r!.Garbage),
+}>({
+  Item: r => Parsimmon.alt(r.Group, r.Garbage),
   Group: r => Parsimmon.string("{")
-    .then(Parsimmon.sepBy(r!.Item, Parsimmon.string(",")))
+    .then(Parsimmon.sepBy(r.Item, Parsimmon.string(",")))
     .skip(Parsimmon.string("}"))
     .map(items => ({ kind: "group", items } as IGroup)),
   Garbage: () => Parsimmon.string("<")
